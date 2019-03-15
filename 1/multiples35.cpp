13308d0e3d48b37550a438a2b94a72e6ccd135bf
Dario Ramos <dario.fenix@gmail.com>
2019-03-15 17:18:23 -0300

    [1][C++] TDD-style

--- 1/multiples35.cpp
+++ 1/multiples35.cpp
@@ -7,15 +7,20 @@ using namespace std;
  * If we multiply side by side by a number, we get all its multiples. For example, for 3:
  *      3 * (1 + 2 + 3 + ... + n) = 3 + 6 + 9 + ... + 3 * n = 3 * n * (n + 1) / 2 
  * */
-int SumDivisibleByN(int n, int max) {
+int sumDivisibleByN(int n, int max) {
     int p = max / n;
     return n * (p * (p + 1)) / 2;
 }
 
-int main() {
-    int max = 999;
+int multiples35(int max) {
     // To avoid adding multiples of 3 and 5 twice, we subtract multiples of 3 * 5 = 15
-    int sum = SumDivisibleByN(3, max) + SumDivisibleByN(5, max) - SumDivisibleByN(15, max);
-    cout << "Sum: " << sum << endl;
+    return sumDivisibleByN(3, max) + sumDivisibleByN(5, max) - sumDivisibleByN(15, max);
+}
+
+int main() {
+    int expectedSum = 233168;
+    int sum = multiples35(999);
+    string message = sum == expectedSum? "PASSED" : "FAILED (expected " + std::to_string(expectedSum) + ", got " + std::to_string(sum) + ")";
+    cout << message << endl;
     return 0;
 }
